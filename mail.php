e356e6ac48c907492a0cf0afc6795838f9efa3ec
Murtaza <65944085+Murtaza100@users.noreply.github.com>
2021-06-07 08:30:22 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,15 +1,19 @@
 <?php
 //get data from form  
+if(isset($_POST['name']) || isset($_POST['email']) || isset($_POST['message']))
+{
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "najmimurtaza2@mail.com";
+
+$to = "najmimurtaza2@gmail.com";
 $subject = "Mail From needygeeks";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@needygeeks.com" . "\r\n" .
 "CC: shamsspd03@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
+}
 }
 //redirect
 header("Location:thankyou.html");
